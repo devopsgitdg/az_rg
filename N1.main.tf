9b3d28fa0bb8d2a5db332e7106efba28cab29a30
hbdevops1 <hugob@outlook.com>
2022-11-17 15:10:41 -0500

    alan's test

--- N1.main.tf
+++ N1.main.tf
@@ -10,7 +10,7 @@ resource "azurerm_resource_group" "rg-wkspc" {
     command = "az group list --query \"{ResourceGroup: '${local.naming_convention.name}'}\""
    
   }
-  
+  # test for alan
   name     = (var.name == "local.naming_convention.name") ? local.naming_convention.name : "${local.naming_convention.name}${format("-%03d", count.index + 1)}" 
   location = var.resource_group_location
   
